415534991137fc6be97e3d989fb7d531ac54bf6c
Christopher <chrisdtna@gmail.com>
2020-06-22 17:15:52 -0300

    Aula de JQuery finalizada

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function consultaCep(){
+  $(".barra-progresso").show();
+  var cep = document.getElementById("cep").value;
+  console.log(cep);
+  var url = "https://viacep.com.br/ws/" + cep + "/json/";
+  console.log(url);
+  $.ajax({
+    url: url,
+    type: "GET",
+    success: function(response){
+      console.log(response);
+      $("#logradouro").html(response.logradouro);
+      //document.getElementById("logradouro").innerHTML = response.logradouro;
+      $("#bairro").html(response.bairro);
+      //document.getElementById("bairro").innerHTML = response.bairro;
+      $("#localidade").html(response.localidade);
+      //document.getElementById("localidade").innerHTML = response.localidade;
+      $("#uf").html(response.uf);
+      //document.getElementById("uf").innerHTML = response.uf;
+      $("#tituloCep").html("CEP " + response.cep);
+      $(".cep").show();
+      $(".barra-progresso").hide();
+    }
+  })
+}
+
+$(function(){
+  $(".cep").hide();
+  $(".barra-progresso").hide();
+});
